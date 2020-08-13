29b2a1499bd81a1049e20151331f2b5437523b83
almond <ali.menta796@gmail.com>
2020-08-13 15:32:04 -0400

    base

---
+++ static/data/data.js
@@ -0,0 +1,266 @@
+data = [
+    {
+        'year': 2019,
+        'months': [
+            {
+                'name': "October",
+                'days':
+                    {
+                        '4': [
+                            {
+                                'event': "$30 pesos subway fare hike."
+                            }
+                        ],
+                        '7': [
+                            {
+                                'event': "High school students organize first subway mass evasion protest."
+                            }
+                        ],
+                        '16': [
+                            {
+                                'event': "Workers have joined high school student subway evasions."
+                            },
+                            {
+                                'event': "Authorities present a bill that will punish severely those who evade subway fare.",
+                                'video': "https://www.youtube.com/watch?v=wXNpjQ_t71w"
+                            }
+                        ],
+                        '18': [
+                            {
+                                'event': "High school student girl is injured by pellet shots. It's the first time a fire arm has been used against protesters in democracy.",
+                                'video': "https://www.youtube.com/watch?v=pfd6qbhWTwo"
+                            },
+                            {
+                                'event': "Subway stations are closed, leaving thousands of workers stranded in the center of the city, who take over the streets to protest alongside students."
+                            },
+                            {
+                                'event': "Meanwhile, president Piñera eats pizza at a restaurant in the rich district of the city."
+                            },
+                            {
+                                'event': 'State of Emergency and curfew is declared for the first time since dictatorship, allowing military to rule the streets. Our parents and grandparents re-live the trauma, as Piñera tells people to "enjoy the rest of the night".'
+                            }
+                        ],
+                        '19': [
+                            {
+                                'event': "Two bodies are found in a fire overnight. Victims are accused of looting and accidentally getting caught in the fire."
+                            },
+                            {
+                                'event': "Peaceful protests have expanded to other cities."
+                            },
+                            {
+                                'event': "Multiple subway stations have suspiciously been subjected to arson at the same time. Long after, it is proved the chemical used is not sold to civilians."
+                            }
+                        ],
+                        '20': [
+                            {
+                                'event': "President Sebastián Piñera declares war against chilean people."
+                            }
+                        ],
+                        '21': [
+                            {
+                                'event': "Several accounts of excessive violence and abuse have been reported. Videos of activists and student leaders being taken from their own homes, their family and friends grab onto them as police drag them towards their van."
+                            },
+                            {
+                                'event': "At least 15 confirmed deaths at the hands of state agents. Many were victims of mysterious fires that ocurred after alleged looting. Forensic reports would later confirm the victims died by bullet wounds, and the doctor who revealed this was removed from her charge."
+                            },
+                            {
+                                'event': "Peaceful protests are repressed with horrible violence. Protesters are shot for no reason."
+                            },
+                            {
+                                'event': "Two victims are tortured by state agents, one of them is a teenager.",
+                                'video': "https://www.youtube.com/watch?v=1ZRMvX9ACY4"
+                            }
+                        ],
+                        '22': [
+                            {
+                                'event': "Another man is tortured in the closed subway station right under the protest site."
+                            },
+                            {
+                                'event': "Piñera announces (weak) social measures to try to appease the discontent, but it's too little too late.",
+                                'mention': [
+                                    'Chileans have been doing peaceful protests ever since the return to democracy in the 90s, students organized in early 2000s for change. The demands were never attended, but riot police conditioned us to normalize the use of water tanks and tear gas for two decades until this moment.'
+                                ]
+                            }
+                        ],
+                        '25': [
+                            {
+                                'event': "More than 1 million people march against the government in Santiago (the capital), similar numbers are mirrored in other cities.",
+                                'pic': "10_25.jpg"
+                            }
+                        ],
+                        '31': [
+                            {
+                                'event': "The ONU arrives in Chile to investigate violations to Human Rights."
+                            },
+                            {
+                                'event': "Amnesty International also condemns the abuse at the hands of chilean state agents."
+                            }
+                        ]
+                    }
+            },
+            {
+                'name': "November",
+                'days': {
+                    '2': [
+                        {
+                            'event': "Protests have taken place every single day, fatal victims ascend to 20."
+                        },
+                        {
+                            'event': 'In 6 days, over 10 thousand working class people have attended self-organized citizen lobbies ("cabildos") to discuss social demands and issues.',
+                            'mention': [
+                                "This is nothing like our previous protests before. Working class people are once again taking part in political discussions. All social fights that were previously demanded separatedly (public health, pensions, gender equality, LGBTQ+ rights, education, children's rights, racism, xenophobia, literally every struggle parts of the population suffered) have been realized by the people to fight as one.",
+                                "The feeling of solidarity and empathy you feel when you step into a chilean protest is something I'm never going to forget. It's the feeling of realizing we were alone in our suffering, but we're not anymore, even if we don't go through the same struggles."
+                            ]
+                        }
+                    ],
+                    '4': [
+                        {
+                            'event': "Guatemalan activist and nobel prize winner Rigoberta Menchú and Joan Jara (widow of Victor Jara) march together in Santiago.",
+                            'pic': "11_4.png"
+                        }
+                    ],
+                    '5': [
+                        {
+                            'event': "President Piñera openly supports the actions of police in an interview with BBC."
+                        }
+                    ],
+                    '7': [
+                        {
+                            'event': "President Piñera announces laws against looting, covering your face and barricades.",
+                            'mention': [
+                                "As protesters, we have figured out ourselves and our systems. Peaceful protests are now dependent of violent protest, but we do not call them that anymore.",
+                                "Front-liners clash and risk their lives containing police brutality so that people can protest and demonstrate behind them safely. Thanks to radical action of those who sacrifice themselves, sometimes with fatal consequences, protesters can express themselves with art performances and political speech.",
+                                "Primera Línea (frontliners) are regarded as heroes of the people."
+                            ],
+                            'pic': "11_7.jpg"
+                        }
+                    ],
+                    '8': [
+                        {
+                            'event': "22 year old college student, Gustavo Gatica, is shot in both eyes and is permanently blinded by police.",
+                            'pic': "11_8.jpg"
+                        },
+                        {
+                            'event': "Eye injuries ascend to 182.",
+                            'video': "https://www.youtube.com/watch?v=rF1sQatbwf0"
+                        }
+                    ],
+                    '9': [
+                        {
+                            'event': "United States white supremacist (and KKK enthusiast) John Cobin opens fire in a beach where protesters demonstrated cheerfully, injuring one."
+                        }
+                    ],
+                    '14': [
+                        {
+                            'event': 'Mon Laferte, who has given continuos support to the protests, attends the Grammys and wins Best Alternative Album. At the red carpet, she courageously takes advantage of the media to deliver the message "In Chile they torture, rape and murder". Her speech is also dedicated to those fighting in her home country, who she would later join.',
+                            'pic': "11_14.jpg"
+                        }
+                    ],
+                    '16': [
+                        {
+                            'event': "It is proven that police has been shooting protesters with ammunition made out of lead, silicon and barium sulfate (sometimes doctors are unable to remove the pellet shots from the body, which means it will contaminate and produce secondary health issues)."
+                        },
+                        {
+                            'event': "Abel Acuña, a protester demonstrating in the peaceful side, dies from cardiac arrest due to tear gas. When medical volunteers try to reanimate him and take him into an ambulance, police attack them with pellet shots and the water tank, while the site is still being gassed.",
+                            'pic': "11_16.jpg"
+                        }
+                    ],
+                    '18': [
+                        {
+                            'event': 'Las Tesis collective releases feminist anthem "Un violador en tu camino" (A rapist in your path) and is first performed in public.'
+                        }
+                    ],
+                    '21': [
+                        {
+                            'event': "Amnesty International finish their visit in Chile. They conclude:",
+                            'quote': "The intention of the Chilean security forces is clear: to injure demonstrators in order to discourage protest, even to the extent of using torture and sexual violence against protesters. Instead of taking measures to curb the very grave human rights crisis, the authorities, under the command of President Sebastián Piñera, have pursued a policy of punishment for over a month, adding yet more people to the staggering number of victims, which is continuing to rise to this day."
+                        },
+                        {
+                            'event': "Chilean government rejects the report and calls it irresponsible."
+                        }
+                    ],
+                    '24': [
+                        {
+                            'event': "President Piñera announces new project to use military force without declaring State of Emergency."
+                        }
+                    ],
+                    '26': [
+                        {
+                            'event': "Fabiola Campillay is hit with a tear gas canister while she was walking to a bus stop to get to her job. She was not a part of the protests. When her sister screamed at cops for help, they shoot a second tear gas in their direction. The injury was so severe she lost both of her eyes, and the sense of smell and taste. She's no longer able to work for her 3 children.",
+                            'pic': "11_26.jpg"
+                        }
+                    ],
+                    '27': [
+                        {
+                            'event': "France refuses to aid chilean police."
+                        }
+                    ]
+                }
+            },
+            {
+                'name': "December",
+                'days': {
+                    '2': [
+                        {
+                            'event': "Piñera's approval rate drops to %10, the lowest any president has ever registered in Chile."
+                        }
+                    ],
+                    '20': [
+                        {
+                            'event': "Oscar Perez is violently crushed between two armored riot cars.",
+                            'pic': "12_20.jpg"
+                        }
+                    ],
+                    '25': [
+                        {
+                            'event': "People celebrate Christmas at the protest site with shared food, as police attack them with water tanks."
+                        }
+                    ],
+                    '27': [
+                        {
+                            'event': "A date for the plebicite to decide whether a new constitution will replace the one inherited from dicatorship is set: April 26th."
+                        },
+                        {
+                            'event': "Frontliner Mauricio Fredes is found dead inside a ditch after the daily police raid. Autopsy confirms the cause of death to be asphyxia by drowning, meaning he died by direct action of the water tank. This place would become a mourning site for protesters that cops destroy every chance they get.",
+                            'pic': "12_27.jpg"
+                        }
+                    ],
+                    '31': [
+                        {
+                            'event': "Chileans celebrate New Year at the protest site, as frontliners protect them from police repression."
+                        }
+                    ]
+                }
+            },
+        ]
+    },
+    {
+        'year': 2020,
+        'months': [
+            {
+                'name': "January",
+                'days': {}
+            },
+            {
+                'name': "February",
+                'days': {
+                    '22': [
+                        {
+                            'event': "Pro-dictatorship fascists march through the streets escolted by police as they attack people who face them. It soon turns into a clash.",
+                        }
+                    ],
+                    '23': [
+                        {
+                            'event': "People protest against a local festival in Valparaíso city, as the country is in no shape for celebrations."
+                        }
+                    ],
+                    '29': [
+                        {
+                            'event': "Pro-dictatorship fascists are allowed to march sporting armor and weapons as they continue to attack civilians."
+                        }
+                    ]
+                }
+            }
+        ]
+    }
+]
---
+++ static/js/app.js
@@ -0,0 +1,48 @@
+function Item(data) {
+    this.picture = ko.computed(function() {
+        return victim_pics[data.name] ? victim_pics[data.name] : victim_pics.default;
+    });
+    this.culprit = ko.computed(function() {
+        return data.esp.attackers.join(", ")
+    });
+    this.name = ko.observable(data.name);
+    this.age = ko.observable(data.age);
+    this.age_cat = ko.observable(data.age_cat);
+    this.date = ko.observable(data.date);
+    this.gob = ko.observable(data.gob);
+    this.labels = ko.observable(data.esp.labels);
+    this.location = ko.observable(data.esp.location);
+    this.background = ko.computed(function(){
+        return data.esp.background == null ? null : data.esp.background.split("\n")
+    })
+    this.death = ko.computed(function(){
+        return data.esp.death.split("\n")
+    })
+    this.aftermath = ko.computed(function(){
+        return data.esp.aftermath == null ? null : data.esp.aftermath.split("\n")
+    })
+    this.attackers = ko.observable(data.esp.attackers);
+    this.sentence = ko.observable(data.esp.sentence);
+    this.status = ko.observable(data.esp.status);
+    this.read_more = ko.observable(data.esp.status);
+}
+
+
+function YearItem(year, items) {
+    this.year = ko.observable(year);
+    this.items = ko.observable(items);
+}
+
+function VictimsModel() {
+    var self = this;
+    self.aa = "djcen"
+    self.mapuche_items = ko.observableArray([]);
+    self.get_items = function() {
+        var years = Object.keys(victims.data)
+        var items = years.map(y => self.mapuche_items.push(new YearItem(y, victims.data[y].map(x => new Item(x)))))
+    }
+    self.get_items()
+}
+
+let victim_model = new VictimsModel()
+ko.applyBindings(victim_model)
